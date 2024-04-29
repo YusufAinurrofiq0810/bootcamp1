ae02eabe4201cdbcb66ded5f4dc088ba9d8770fa
YusufAinurrofiq0810 <120088479+YusufAinurrofiq0810@users.noreply.github.com>
2024-04-29 15:17:24 +0700

    apalah

---
+++ belajar/html/slicing/slicing.js
@@ -0,0 +1,11 @@
+function showMenuToggle(toggleId, menuId) {
+    const menuToggle = document.getElementById(toggleId);
+    const menuID = document.getElementById(menuId);
+  
+    menuToggle.addEventListener('click', () => {
+      menuID.classList.toggle('show-menu');
+      menuToggle.classList.toggle('show-icon');
+    });
+  }
+  
+  showMenuToggle('toggle-menu', 'navbar-menu');
